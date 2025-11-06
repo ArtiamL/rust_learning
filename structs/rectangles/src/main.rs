db9f74dac3cf045172ed351dd065e54fa7d2072c
ArtiamL <main.lnewell@gmail.com>
2025-11-06 00:01:35 +0000

    Rust book ch5.3: (rectangles) changed area function to Rectangle method

--- structs/rectangles/src/main.rs
+++ structs/rectangles/src/main.rs
@@ -4,6 +4,12 @@ struct Rectangle {
     height: u32,
 }
 
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+}
+
 fn main() {
     let scale = 2;
 
@@ -12,9 +18,8 @@ fn main() {
         height: 50,
     };
 
-    dbg!(&rect1);
-}
-
-fn area(rectangle: &Rectangle) -> u32 {
-    rectangle.width * rectangle.height
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        rect1.area()
+    );
 }
