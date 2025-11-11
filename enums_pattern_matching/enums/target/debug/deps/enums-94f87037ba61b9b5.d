cb187c042b71f3a9cdbfe044a32d60db4a88f6cb
ArtiamL <main.lnewell@gmail.com>
2025-11-11 16:35:57 +0000

    Rust book ch6.1: Enum created with struct for address data

---
+++ enums_pattern_matching/enums/target/debug/deps/enums-94f87037ba61b9b5.d
@@ -0,0 +1,9 @@
+/home/artiaml/dev/rust/learning/enums_pattern_matching/enums/target/debug/deps/enums-94f87037ba61b9b5.d: src/main.rs Cargo.toml
+
+/home/artiaml/dev/rust/learning/enums_pattern_matching/enums/target/debug/deps/libenums-94f87037ba61b9b5.rmeta: src/main.rs Cargo.toml
+
+src/main.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=--no-deps__CLIPPY_HACKERY__
+# env-dep:CLIPPY_CONF_DIR
