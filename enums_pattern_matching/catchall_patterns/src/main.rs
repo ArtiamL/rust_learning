e62bb0a5f46cfbfc8d87d8c3a567e0868bdf7e3e
ArtiamL <main.lnewell@gmail.com>
2025-12-04 15:35:01 +0000

    Rust book ch06.2: Catch-All Patterns & _ placeholder

---
+++ enums_pattern_matching/catchall_patterns/src/main.rs
@@ -0,0 +1,14 @@
+fn main() {
+    let dice_roll = 9;
+    match dice_roll {
+        3 => add_fancy_hat(),
+        7 => remove_fancy_hat(),
+        other: move_player(),
+    }
+}
+
+fn add_fancy_hat() {}
+
+fn remove_fancy_hat() {}
+
+fn move_player(num_spaces: u8) {}
