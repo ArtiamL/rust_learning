08dbed8fb3d196f552efdcce39b6b9b78b20f3a9
ArtiamL <main.lnewell@gmail.com>
2025-12-04 16:21:36 +0000

    Rust book ch06.2: Changed _ catch-all to use unit value '()' as nothing now will happen

--- enums_pattern_matching/catchall_patterns/src/main.rs
+++ enums_pattern_matching/catchall_patterns/src/main.rs
@@ -3,12 +3,10 @@ fn main() {
     match dice_roll {
         3 => add_fancy_hat(),
         7 => remove_fancy_hat(),
-        _ => reroll(),
+        _ => (),
     }
 }
 
 fn add_fancy_hat() {}
 
 fn remove_fancy_hat() {}
-
-fn reroll() {}
