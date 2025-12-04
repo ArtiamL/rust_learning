33fea30cee366eb551528ed02c85673d43ca8577
ArtiamL <main.lnewell@gmail.com>
2025-12-04 16:51:44 +0000

    Rust book ch06.3: Added `if let` for more concise single match expressions

--- enums_pattern_matching/concise_flow/src/main.rs
+++ enums_pattern_matching/concise_flow/src/main.rs
@@ -1,7 +1,14 @@
 fn main() {
-    let config_max = Some(3u8);
-    match config_max {
+    // Single case match, lots of boilerplate
+    let o_config_max = Some(3u8);
+    match o_config_max {
         Some(max) => println!("The maximum is configured to be {max}"),
         _ => (),
+    }
+
+    // If let - syntactic sugar for more concise for single-use match expr
+    let config_max = Some(3u8);
+    if let Some(max) = config_max {
+        println!("The maximum is configured to be {max}");
     }
 }
