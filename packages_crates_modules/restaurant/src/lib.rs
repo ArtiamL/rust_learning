741936af1ab7eee4f95287dc0ff255681cb3ad47
ArtiamL <main.lnewell@gmail.com>
2025-12-16 16:43:48 +0000

    Rust book ch07.3: Added public breakfast struct to aid in understanding public structs & enums.

--- packages_crates_modules/restaurant/src/lib.rs
+++ packages_crates_modules/restaurant/src/lib.rs
@@ -15,11 +15,14 @@ mod front_of_house {
 }
 
 pub fn eat_at_restaurant() {
-    // Absolute Path
-    crate::front_of_house::hosting::add_to_waitlist();
+    // Order a breakfast in the summer with Rye toast.
+    let mut meal = back_of_house::Breakfast::summer("Rye");
+    // Change our mind about what bread we'd like.
+    meal.toast = String::from("Wheat");
+    println!("I'd like {} toast please", meal.toast);
 
-    // Relative Path
-    front_of_house::hosting::add_to_waitlist();
+    // The next line won't compile if uncommented as seasonal_fruit is private
+    // meal.seasonal_fruit = String::from("blueberries");
 }
 
 fn deliver_order() {}
@@ -31,4 +34,18 @@ mod back_of_house {
     }
 
     fn cook_order() {}
+
+    pub struct Breakfast {
+        pub toast: String,
+        seasonal_fruit: String,
+    }
+
+    impl Breakfast {
+        pub fn summer(toast: &str) -> Breakfast {
+            Breakfast {
+                toast: String::from(toast),
+                seasonal_fruit: String::from("peaches"),
+            }
+        }
+    }
 }
