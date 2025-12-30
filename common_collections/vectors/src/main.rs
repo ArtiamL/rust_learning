a725b188f387c066802f730c0cc92478ab67f418
ArtiamL <main.lnewell@gmail.com>
2025-12-30 17:09:50 +0000

    Rust book ch08.1: Utilised an enum to store different types inside a vector, added a comment to indicate vector freeing at end of scope.

--- common_collections/vectors/src/main.rs
+++ common_collections/vectors/src/main.rs
@@ -34,4 +34,18 @@ fn main() {
     for i in &mut v {
         *i += 50;
     }
+
+    let row = vec![
+        SpreadsheetCell::Int(3),
+        SpreadsheetCell::Text(String::from("blue")),
+        SpreadsheetCell::Float(10.12),
+    ];
+
+    // Additional comments: Vectors are freed when out of scope
+}
+
+enum SpreadsheetCell {
+    Int(i32),
+    Float(f64),
+    Text(String),
 }
