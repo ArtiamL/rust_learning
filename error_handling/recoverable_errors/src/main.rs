1dbd215c410c6d52dfe3f9612496a12c24970fa4
ArtiamL <main.lnewell@gmail.com>
2026-01-08 17:02:30 +0000

    Rust book ch09.2: Added section on propogating errors.

--- error_handling/recoverable_errors/src/main.rs
+++ error_handling/recoverable_errors/src/main.rs
@@ -1,4 +1,8 @@
-use std::{fs::File, io::ErrorKind};
+#![allow(unused_variables)]
+use std::{
+    fs::File,
+    io::{self, ErrorKind, Read},
+};
 
 fn main() {
     let greeting_file_result = File::open("hello.txt");
@@ -15,4 +19,34 @@ fn main() {
             }
         },
     };
+
+    // unwrap() creates a panic!() call automatically.
+    let greeting_file_unwrap = File::open("unwrap.txt").unwrap();
+
+    // expect() allows for custom panic!() error messages.
+    let greeting_file_expect =
+        File::open("expect.txt").expect("expect.txt should be included in this project!");
+}
+
+fn read_username_from_file() -> Result<String, io::Error> {
+    let username_file_result = File::open("username.txt");
+
+    let mut username_file = match username_file_result {
+        Ok(file) => file,
+        Err(e) => return Err(e),
+    };
+
+    let mut username = String::new();
+
+    match username_file.read_to_string(&mut username) {
+        Ok(_) => Ok(username),
+        Err(e) => Err(e),
+    }
+}
+
+fn read_username_from_file() -> Result<String, io::Error> {
+    let mut username_file = File::open("username.txt")?;
+    let mut username = String::new();
+    username_file.read_to_string(&mut username)?;
+    Ok(username)
 }
