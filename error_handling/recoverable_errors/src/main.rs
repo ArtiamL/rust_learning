64dfe180b027f80994d226bae232a105c73d4349
ArtiamL <main.lnewell@gmail.com>
2026-01-14 17:43:49 +0000

    Rust book ch09.3: Finished chapter on Error Handling

--- error_handling/recoverable_errors/src/main.rs
+++ error_handling/recoverable_errors/src/main.rs
@@ -1,6 +1,6 @@
-#![allow(unused_variables)]
+#![allow(unused_variables, dead_code, clippy::question_mark)]
 use std::{
-    fs::File,
+    fs::{self, File},
     io::{self, ErrorKind, Read},
 };
 
@@ -28,6 +28,7 @@ fn main() {
         File::open("expect.txt").expect("expect.txt should be included in this project!");
 }
 
+// Error propogation, long-way
 fn read_username_from_file() -> Result<String, io::Error> {
     let username_file_result = File::open("username.txt");
 
@@ -44,9 +45,25 @@ fn read_username_from_file() -> Result<String, io::Error> {
     }
 }
 
-fn read_username_from_file() -> Result<String, io::Error> {
+// Error propogation, using ? shortcut
+fn read_username_from_file_shortcut() -> Result<String, io::Error> {
     let mut username_file = File::open("username.txt")?;
     let mut username = String::new();
     username_file.read_to_string(&mut username)?;
     Ok(username)
+}
+
+// Error propogation with chain operation on ? shortcut
+fn read_username_from_file_chain() -> Result<String, io::Error> {
+    let mut username = String::new();
+
+    File::open("username_chain.txt")?.read_to_string(&mut username)?;
+
+    Ok(username)
+}
+
+// fs::read_to_string, shortest way to read from file. Has same error handling &
+// propogation as previous functions.
+fn read_username_from_file_fs() -> Result<String, io::Error> {
+    fs::read_to_string("username_fs.txt")
 }
