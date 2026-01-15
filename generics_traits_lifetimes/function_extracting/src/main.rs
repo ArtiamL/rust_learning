d294b0f440b3450c90891260fd66909d07d825f2
ArtiamL <main.lnewell@gmail.com>
2026-01-15 15:36:05 +0000

    Rust book ch10.1: Added PartialOrd Trait to restrict valid types to those implementing PartialOrd.

--- generics_traits_lifetimes/function_extracting/src/main.rs
+++ generics_traits_lifetimes/function_extracting/src/main.rs
@@ -1,4 +1,4 @@
-fn largest<T>(list: &[T]) -> &T {
+fn largest<T: std::cmp::PartialOrd>(list: &[T]) -> &T {
     let mut largest = &list[0];
 
     for item in list {
@@ -13,11 +13,11 @@ fn largest<T>(list: &[T]) -> &T {
 fn main() {
     let number_list = vec![34, 50, 25, 100, 65];
 
-    let result = largest_i32(&number_list);
+    let result = largest(&number_list);
     println!("The largest number is {result}");
 
     let char_list = vec!['y', 'm', 'a', 'q'];
 
-    let result = largest_char(&char_list);
+    let result = largest(&char_list);
     println!("The largest char is {result}");
 }
