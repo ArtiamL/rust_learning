4a18cd35176cc72a0a8f5340aaedfea3709404b9
ArtiamL <main.lnewell@gmail.com>
2026-01-15 16:16:09 +0000

    Rust book ch10.1: Implemented `mixup()` for Point<X1, Y1>.

--- generics_traits_lifetimes/function_extracting/src/main.rs
+++ generics_traits_lifetimes/function_extracting/src/main.rs
@@ -10,35 +10,25 @@ fn largest<T: std::cmp::PartialOrd>(list: &[T]) -> &T {
     largest
 }
 
-struct Point<T> {
-    x: T,
-    y: T,
+struct Point<X1, Y1> {
+    x: X1,
+    y: Y1,
 }
 
-impl<T> Point<T> {
-    fn x(&self) -> &T {
-        &self.x
-    }
-}
-
-impl Point<f32> {
-    fn distance_from_origin(&self) -> f32 {
-        (self.x.powi(2) + self.y.powi(2)).sqrt()
+impl<X1, Y1> Point<X1, Y1> {
+    fn mixup<X2, Y2>(self, other: Point<X2, Y2>) -> Point<X1, Y2> {
+        Point {
+            x: self.x,
+            y: other.y,
+        }
     }
 }
 
 fn main() {
-    let number_list = vec![34, 50, 25, 100, 65];
+    let p1 = Point { x: 5, y: 10.4 };
+    let p2 = Point { x: "Hello", y: 'c' };
 
-    let result = largest(&number_list);
-    println!("The largest number is {result}");
+    let p3 = p1.mixup(p2);
 
-    let char_list = vec!['y', 'm', 'a', 'q'];
-
-    let result = largest(&char_list);
-    println!("The largest char is {result}");
-
-    let both_integer = Point { x: 5, y: 10 };
-    let both_float = Point { x: 1.0, y: 4.0 };
-    let integer_and_float = Point { x: 5, y: 4.0 };
+    println!("p3.x = {}, p3.y = {}", p3.x, p3.y);
 }
