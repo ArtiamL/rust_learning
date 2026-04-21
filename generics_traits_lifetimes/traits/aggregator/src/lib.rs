46afa1f0097e6811131bddab512ea150fd63fac6
ArtiamL <main.lnewell@gmail.com>
2026-04-21 17:48:40 +0100

    Rust book ch10.2: Finished trait chapter with look at conditional traits.

--- generics_traits_lifetimes/traits/aggregator/src/lib.rs
+++ generics_traits_lifetimes/traits/aggregator/src/lib.rs
@@ -1,3 +1,8 @@
+use std::{
+    fmt::{Debug, Display},
+    i32,
+};
+
 pub trait Summary {
     fn summarize_author(&self) -> String;
 
@@ -36,6 +41,53 @@ impl Summary for SocialPost {
     }
 }
 
-pub fn notify<T: Summary>(item: &T) {
+// NOTE: Both fn do the same, this shows the different ways of doing trait bounds.
+pub fn notify(item: &(impl Summary + Display)) {
     println!("Breaking news! {}", item.summarize());
+}
+
+// pub fn notify<T: Summary + Display>(item: &T) {
+//     println!("Breaking news! {}", item.summarize());
+// }
+
+// Multiple different trait bounds can be done with `where` clause
+fn some_function<T, U>(t: &T, u: &U) -> i32
+where
+    T: Display + Clone,
+    U: Clone + Debug,
+{
+    // This is just to prevent errors, body of fn is irrelevant as this just shows where clause.
+    i32::MAX
+}
+
+fn returns_summarizable() -> impl Summary {
+    SocialPost {
+        username: String::from("horse_ebooks"),
+        content: String::from("of course, as you probably already know, people"),
+        reply: false,
+        repost: false,
+    }
+}
+
+struct Pair<T> {
+    x: T,
+    y: T,
+}
+
+impl<T> Pair<T> {
+    fn new(x: T, y: T) -> Self {
+        Self { x, y }
+    }
+}
+
+// Conditional impl only implements cmp_display for Pairs with inner type Ts that implement both
+// Display and PartialOrd
+impl<T: Display + PartialOrd> Pair<T> {
+    fn cmp_display(&self) {
+        if self.x >= self.y {
+            println!("The largest member is x = {}", self.x);
+        } else {
+            println!("The largest member is y = {}", self.y);
+        }
+    }
 }
