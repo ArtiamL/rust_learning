80f8a2f00bf9286805277314e21ad3b49aa0e183
ArtiamL <main.lnewell@gmail.com>
2026-05-27 15:31:43 +0100

    Rust book ch11.1: Finished section on How to write tests.

--- automated_testing/adder/src/lib.rs
+++ automated_testing/adder/src/lib.rs
@@ -14,9 +14,28 @@ pub fn add(left: u64, right: u64) -> u64 {
     left + right
 }
 
+pub fn add_two(a: u64) -> u64 {
+    a + 2
+}
+
+pub fn greeting(name: &str) -> String {
+    String::from("Hello!")
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    fn it_works() -> Result<(), String> {
+        let result = add(2, 2);
+
+        if result == 4 {
+            Ok(())
+        } else {
+            Err(String::from("two plus two does not equal four"))
+        }
+    }
 
     #[test]
     fn exploration() {
@@ -42,5 +61,35 @@ mod tests {
         };
 
         assert!(larger.can_hold(&smaller));
+    }
+
+    #[test]
+    fn smaller_cannot_hold_larger() {
+        let larger = Rectangle {
+            width: 8,
+            height: 7,
+        };
+
+        let smaller = Rectangle {
+            width: 5,
+            height: 1,
+        };
+
+        assert!(!smaller.can_hold(&larger));
+    }
+
+    #[test]
+    fn it_adds_two() {
+        let result = add_two(2);
+        assert_eq!(result, 4);
+    }
+
+    #[test]
+    fn greeting_contains_name() {
+        let result = greeting("Carol");
+        assert!(
+            result.contains("Carol"),
+            "Greeting did not contain name, value was `{result}`"
+        );
     }
 }
